42a4df3fd4de5ec5e76bdcd232b5660e0a8d3c3a
O-S Marin <K07H@users.noreply.github.com>
2023-03-08 13:31:06 +0100

    Added base64 copyright notices
    * Added base64 copyright notices (even tho this class is not used in YUMI yet).

--- YUMI/base64.cpp
+++ YUMI/base64.cpp
@@ -73,6 +73,30 @@
 ** Franklin Street, Fifth Floor, Boston, MA 02110 USA.
 */
 
+/* Copyright (c) 2016-2019 Vinnie Falco (vinnie dot falco at gmail dot com)
+ * Distributed under the Boost Software License, Version 1.0. (See accompanying
+ * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ * Official repository: https://github.com/boostorg/beast */
+
+/* Portions from http://www.adp-gmbh.ch/cpp/common/base64.html
+ * Copyright notice:
+ * base64.cpp and base64.h
+ * Copyright (C) 2004-2008 Rene Nyffenegger
+ * This source code is provided 'as-is', without any express or implied
+ * warranty. In no event will the author be held liable for any damages
+ * arising from the use of this software.
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 1. The origin of this source code must not be misrepresented; you must not
+ *    claim that you wrote the original source code. If you use this source code
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original source code.
+ * 3. This notice may not be removed or altered from any source distribution.
+ * Rene Nyffenegger rene.nyffenegger@adp-gmbh.ch */
+
 #include "base64.h"
 
 const char* Base64::b64GetAlphabet()
